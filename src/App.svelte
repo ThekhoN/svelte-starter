c81c7316f4785197ac0b83b6fb33da98241b8e6e
Ngaosathe Thekho <bennetonss@hotmail.com>
2020-05-03 20:30:10 +0530

    initial counter example

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Counter from './Counter.svelte';
+	let count = 9
+</script>
+
+<main>
+	<Counter count={count} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let count = 0;
+</script>
+
+<div>
+  <button on:click="{() => count -= 1}">-</button>
+  <h2>{count}</h2>
+  <button on:click="{() => count += 1}">+</button>
+</div>
+
+<style>
+	div {
+		display: flex;
+	}
+
+	button {
+		margin: 1rem;
+	}
+</style>
